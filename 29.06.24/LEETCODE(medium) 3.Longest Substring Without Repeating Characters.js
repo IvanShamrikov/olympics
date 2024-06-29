9d0171f916babcd10ef3c271b6139883f3d0c708
Oleg Shvydkiy <oleg.mason8686@gmail.com>
2024-06-29 18:33:39 +0300

    29.06.2024

---
+++ 29.06.24/LEETCODE(medium) 3.Longest Substring Without Repeating Characters.js
@@ -0,0 +1,33 @@
+// Given a string s, find the length of the longest substring without repeating characters
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+var lengthOfLongestSubstring = function (s) {
+    let temp = [];
+    let biggest = s.length > 0 ? 1 : 0;
+    for (let i = 0; i < s.length; i++) {
+        if (temp.includes(s[i])) {
+            temp.splice(0, temp.indexOf(s[i]) + 1)
+        }
+        temp.push(s[i]);
+
+        if (biggest < temp.length) biggest = temp.length
+    }
+    return biggest;
+};
