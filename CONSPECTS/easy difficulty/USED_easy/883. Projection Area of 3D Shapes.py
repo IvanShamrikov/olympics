f00be122b68b0468cd5811b88a750535d2cc1832
Shamrikov <ivan.shamrikov@gmail.com>
2024-10-06 14:32:56 +0300

    lesson 5

--- CONSPECTS/easy difficulty/USED_easy/883. Projection Area of 3D Shapes.py
+++ CONSPECTS/easy difficulty/USED_easy/883. Projection Area of 3D Shapes.py
@@ -1,15 +1,15 @@
  # Picture: https://leetcode.com/problems/projection-area-of-3d-shapes/description/
  #
- # You are given an n x n grid where we place some 1 x 1 x 1 cubes that are axis-aligned with the x, y, and z axes.
- # Each value v = grid[i][j] represents a tower of v cubes placed on top of the cell (i, j).
- # We view the projection of these cubes onto the xy, yz, and zx planes.
- # A projection is like a shadow, that maps our 3-dimensional figure to a 2-dimensional plane.
- # We are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
- # Return the total area of all three projections.
+ # Вам дано сітку розміром n x n, де ми розміщуємо деякі кубики розміром 1 x 1 x 1, які вирівняні по осях x, y та z.
+ # Кожне значення v = grid[i][j] представляє вежу з v кубиків, розміщених на клітинці (i, j).
+ # Ми спостерігаємо проекцію цих кубиків на площини xy, yz та zx.
+ # Проекція подібна до тіні, яка відображає нашу тривимірну фігуру на двовимірну площину.
+ # Ми бачимо "тінь", коли дивимося на кубики зверху, спереду та збоку.
+ # Поверніть загальну площу всіх трьох проекцій.
  #
  # Input: grid = [[1,2],[3,4]]
  # Output: 17
- # Explanation: Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+ # Explanation: Ось три проекції ("тіні") фігури, утвореної на кожній площині, вирівняній за осями.
  #
  # Example 2:
  # Input: grid = [[2]]
@@ -41,9 +41,9 @@ for i in range(len(grid)):
             xy += 1
             print(f"Тут стоїть дім висотою {grid[i][j]}. По підлозі xy він займає 1 клітину. Додаємо +1 до загального лічільника тіней по землі XУ ")
         else:
-            print(f"Тут стоїть дім висотою {grid[i][j]}. Тобто дім не стоїть, тіні неа землю не відбрасує, нічого не додаємо до загального лічільника тіней по землі XУ ")
+            print(f"Тут дім не стоїть, тіні неа землю не відбрасує, нічого не додаємо до загального лічільника тіней по землі XУ ")
 
-        print(f"Ідемо стовпчику {j} і рядку {i}")
+        print(f"Ідемо по стовпчику {j} і рядку {i}")
         if grid[j][i] > maximum:
             print(f"Тут стоїть дім висотою {grid[j][i]}. Його висота поки є найбільшою серед вертикалі, тому запамятаємо його тінь як найвищу поки шо")
             maximum = grid[j][i]
@@ -52,6 +52,7 @@ for i in range(len(grid)):
 
 
     yz += maximum
+    print("-"*25)
 
 print(f"xy = ", xy)
 print(f"xz = ", xz)
